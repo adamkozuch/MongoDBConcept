b88aab6311594b8dc04568af72822c474bc01254
adam <adam.kozuch@gmail.com>
2015-03-22 17:09:53 +0100

    fix

--- slickNestedDocument.scala
+++ slickNestedDocument.scala
@@ -1,6 +1,12 @@
 
 
-It will be neccesery to enforce schema to keep typesafety.
+
+"creator" : {
+                  "$ref" : "creators",
+                  "$id" : ObjectId("5099803df3f4948bd2f98391"),
+                  "$db" : "users"
+               }
+
 
 
 {
@@ -12,40 +18,73 @@ It will be neccesery to enforce schema to keep typesafety.
                views : NumberLong(1250000)
 }
 
-class Nazwa extends Document[Int,Document,Date,Date,String*, Document](tag,nameOfCollection)
+
+
+
+
+class creators extends Document[Int,Document,String*](tag,nameOfCollection)
 {
      //this is already implemented
-     def contribs = collumn[Int]("id");
-     def id = collumn[Int]("id");
-     //nested document not implemented
-     def name = document[String,String]("name")
-     {
-     def first = collumn[String]("id");
-     def last = collumn[String]("id")
-     }
-     //teoretycznie powinno byc date
-     def birth = collumn[Int]("id");
-     def death = collumn[Int]("id");
+     def id:Rep[] = collumn[Int]("id");
 
-     //wazne jek sobie poradzić z tablicami
-     def contribs* = collumn[String]("contribs"); //tutaj trzeba oznaczyc ze moze byc wiele
+     //nested document not implemented yet
+     def name:Rep[Document] = new Document[String,String](id_of_document)
+         {
+             def first:Rep[String] = collumn[String]("first");
+             def last:Rep[String] = collumn[String]("last")
+         }
 
-     def creator= Document[ref]{
-def ref
-def id
-def db
+     //how to handle tables
+     def contribs(*):Rep[String*] = collumn[String*]("contribs"); //star mean that this is an array
+
+     def * =(id,(first,last),contribs*)
 }
 
-And now we define projection
-def * (contribs,id,name,birth,death,contribs*,creator)
+val db = Database.forURL("mongodb://localhost:27017/test")
 
+try {
+
+    // The query interface for the creators collection
+    val creators: DocumentQuery[creator] = DocumentQuery[creator]
+     val setupAction: DBIO[Unit] = DBIO.seq(
+     // create the schema of document
+    creators.schema.create,
+    suppliers += (1, ("Thomas", "Edison"),["bulb","electricity"]),
+    suppliers += ( 2, ("Nikola", "Tesla"), ["wireless lighting", "induction motor", "AC dynamo-electric machine","radio-controlled boat"]),
+    suppliers += (3, ("Henry", "Ford"), ["car", "engine"])
+    )
+
+  val setupFuture: Future[Unit] = db.run(setupAction)
+
+  // Insert some coffees (using JDBC's batch insert feature)
+
+  val f = setupFuture.flatMap { _ =>
+
+
+
+      val insertAction =
+      creators += Seq (//
+       (4, ("Socrates",None),[])  //I believe if some field has no value we should provide None
+     )
+
+
+  }
 }
+
+
+
+
+
+
+
+
+
 
 
 
 val nawa_query = TableQuery[Nazwa] // table query też prawdopodobnie powinna być zmodyfikowana
 
-nazwa_query += (1,"John",123L)
+nazwa_query += (1,"John",123L)  ważne jest że jeśli wprowadzimy blędne dane tutaj to kompilator wyrzuci błąd
 
 
 
