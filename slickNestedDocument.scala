ae16f916eed23995f2398a26e832a50a7fe37593
adam.kozuch@gmail.com <adam.kozuch@gmail.com>
2015-03-22 17:17:04 +0100

    j

--- slickNestedDocument.scala
+++ slickNestedDocument.scala
@@ -1,22 +1,6 @@
 
 
 
-"creator" : {
-                  "$ref" : "creators",
-                  "$id" : ObjectId("5099803df3f4948bd2f98391"),
-                  "$db" : "users"
-               }
-
-
-
-{
-               _id: ObjectId("5099803df3f4948bd2f98391"),  // check how it would be with ObjectId for now simple int
-               name: { first: "Alan", last: "Turing" },
-               birth: new Date('Jun 23, 1912'),
-               death: new Date('Jun 07, 1954'),
-               contribs: [ "Turing machine", "Turing test", "Turingery" ],
-               views : NumberLong(1250000)
-}
 
 
 
